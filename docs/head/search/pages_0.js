173a7655b068558e22cd86e895418addc9ec6e82
Travis CI <travis@travis-ci.org>
2018-11-02 20:14:55 +0000

    Deploy code docs to GitHub Pages Travis build: 8
    Commit: fe228547b9129bdee8bf199c80854cf472fb449a

--- docs/head/search/pages_0.js
+++ docs/head/search/pages_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['lightweight_20platform_20abstraction',['LightWeight Platform Abstraction',['../md_mainpage.html',1,'']]]
+  ['lightweight_20platform_20abstraction',['LightWeight Platform Abstraction',['../index.html',1,'']]]
 ];
