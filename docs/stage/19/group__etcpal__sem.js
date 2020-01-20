4e57e86f5ffb948e0537e512f665cda6a79ad263
ETCLabs CI <noreply.etclabs@etcconnect.com>
2020-01-20 23:13:39 +0000

    Deploy code docs to GitHub Pages
    Azure build: 20200120.4
    
    Commit: 73efb4c1ea66ed0b9ceb0e47f87766230799836a

--- docs/stage/19/group__etcpal__sem.js
+++ docs/stage/19/group__etcpal__sem.js
@@ -3,8 +3,8 @@ var group__etcpal__sem =
     [ "etcpal_sem_t", "group__etcpal__sem.html#ga90664f75ae59f03b85b45cc19d3a55ef", null ],
     [ "etcpal_sem_create", "group__etcpal__sem.html#ga5f8de05c22efb1e25bf24890b4afb21d", null ],
     [ "etcpal_sem_destroy", "group__etcpal__sem.html#gaa4d8fce64dfa93c4c2f7bd08100ef8b0", null ],
-    [ "etcpal_sem_post", "group__etcpal__sem.html#ga0ce5c783d7d7fdf566c20a6855132cf8", null ],
-    [ "etcpal_sem_post_from_isr", "group__etcpal__sem.html#ga1b8736a58e66ee3d243d7e7185996dad", null ],
+    [ "etcpal_sem_post", "group__etcpal__sem.html#ga9e39fbcf982a2d058c7b3f1c598d6aac", null ],
+    [ "etcpal_sem_post_from_isr", "group__etcpal__sem.html#gacd9bdeefdee24e47f40090840665c4fb", null ],
     [ "etcpal_sem_timed_wait", "group__etcpal__sem.html#gad2f3af8fc26a2d028edd4c1341304061", null ],
     [ "etcpal_sem_try_wait", "group__etcpal__sem.html#ga4e6901853d69d55f97717f14bb6eb290", null ],
     [ "etcpal_sem_wait", "group__etcpal__sem.html#gadf3cdb9d9d92dfde1b9933d6076e63aa", null ]
