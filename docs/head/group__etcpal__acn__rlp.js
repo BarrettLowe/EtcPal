3781007be25a92c50b602dc68f2b09197de784d6
ETCLabs CI <noreply.etclabs@etcconnect.com>
2020-02-27 17:05:46 +0000

    Deploy code docs to GitHub Pages
    Azure build: 20200227.4
    
    Commit: 4f82935f6d128ea64da5b60e7c87adfbf03c34f7

--- docs/head/group__etcpal__acn__rlp.js
+++ docs/head/group__etcpal__acn__rlp.js
@@ -9,7 +9,7 @@ var group__etcpal__acn__rlp =
       [ "rlp_block_len", "struct_acn_udp_preamble.html#ab27f79ca664937f91e64f868ad233935", null ]
     ] ],
     [ "AcnRootLayerPdu", "struct_acn_root_layer_pdu.html", [
-      [ "datalen", "struct_acn_root_layer_pdu.html#a3d12e161726566d24a1d0cf6c6b416bb", null ],
+      [ "data_len", "struct_acn_root_layer_pdu.html#ace1c2e33b74df8973a7d9a19c935af80", null ],
       [ "pdata", "struct_acn_root_layer_pdu.html#a2f9eb29aa10e0d64f27f69db1e90bd07", null ],
       [ "sender_cid", "struct_acn_root_layer_pdu.html#a341064da4798492e212c14a9aeede58d", null ],
       [ "vector", "struct_acn_root_layer_pdu.html#a513d751c7097b43b3968ac81f3a5715b", null ]
