6b28a99362734b225a88d367183d560ca18fd38f
ETCLabs CI <noreply.etclabs@etcconnect.com>
2020-03-20 19:20:14 +0000

    Deploy code docs to GitHub Pages
    Azure build: 20200320.1
    
    Commit: 38b6a24db586b85495cf6972e3618cf53b7668f1

---
+++ docs/stage/29/modules.js
@@ -0,0 +1,5 @@
+var modules =
+[
+    [ "etcpal (Core Modules)", "group__etcpal.html", "group__etcpal" ],
+    [ "etcpal/cpp (C++ Wrappers)", "group__etcpal__cpp.html", "group__etcpal__cpp" ]
+];
---
+++ docs/stage/29/navtreedata.js
@@ -0,0 +1,27 @@
+var NAVTREE =
+[
+  [ "EtcPal", "index.html", [
+    [ "ETC Platform Abstraction Layer", "index.html", null ],
+    [ "Modules", "modules.html", "modules" ],
+    [ "Files", null, [
+      [ "File List", "files.html", "files" ],
+      [ "Globals", "globals.html", [
+        [ "All", "globals.html", null ],
+        [ "Macros", "globals_defs.html", null ]
+      ] ]
+    ] ],
+    [ "Additional Documentation", "additional_docs.html", "additional_docs" ],
+    [ "Building EtcPal", "building_etcpal.html", "building_etcpal" ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"acn__pdu_8h_source.html",
+"classetcpal_1_1_timer.html#a17896e888578cea83217f4bbef07559a",
+"group__etcpal__inet.html#ga56a6933874bc52770eac5865f237e225",
+"group__etcpal__socket.html#ga653ed9405ec4cb21d958faab8b1c783e"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
